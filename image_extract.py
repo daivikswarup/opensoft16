3fc509878439517db31962d84f9154cdf087fc77
A.V.S.Chaithanya <avs.iitkgp@gmail.com>
2016-03-04 22:08:59 +0530

    cleaned up things little bit

--- image_extract.py
+++
@@ -1,6 +0,0 @@
-#requires ghosht script
-import subprocess
-
-pdf_path = "a.pdf"
-Extraction = subprocess.call("gs -sDEVICE=pngalpha -o file-%03d.png -sDEVICE=pngalpha -r144 " +pdf_path,shell=True)
-print 
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import re
 
 #requires Ghost Script installed in system Which is default in Ubuntu
 def process(pdf_path="a.pdf",job_number = 1):	#pdf_path is pdf name in current directory and job_number is for processing multiple pdf files
-	Extraction = subprocess.check_output("gs -sDEVICE=jpeg -o file-"+ str(job_number) +"-%03d.jpg -r144 " +pdf_path,shell=True)
-	num_vector = map(int, re.findall(r'\d+', Extraction))
-	pages_processed = num_vector[len(num_vector)-1]
-	return pages_processed
+	Extraction = subprocess.check_output("gs -sDEVICE=jpeg -o file-"+ str(job_number) +"-%03d.jpg -r144 " +pdf_path,shell=True);
+	num_vector = map(int, re.findall(r'\d+', Extraction));
+	pages_processed = num_vector[len(num_vector)-1];
+	return pages_processed;
 
 def __main__():
 	# This function process all the pdfs in the docList
